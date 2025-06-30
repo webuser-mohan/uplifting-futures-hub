bbf78e62f22acc7f5a42e94a3d4aff6dc1579470
webuser-mohan <mrajbmm005@gmail.com>
2025-06-30 20:29:51 +0530

    Prepare for GitHub Pages deployment3

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
-
-export default defineConfig({
-  base: "/uplifting-futures-hub/", // <-- your repo name
-  plugins: [react(), tsconfigPaths()],
-});
